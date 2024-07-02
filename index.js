61b18f3026af0db765a2f1d234d9e5c5a60df5a5
Nischal Neupane <nischalneupane17@gmail.com>
2024-07-02 09:51:30 +0545

    made minor changes

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const dns = require('dns');
-const { URL } = require('url');
-
 const app = express();
+const dns = require('dns');
+const URL = require('url').URL;
+
+// Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
@@ -18,6 +19,7 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
@@ -25,50 +27,53 @@ app.get('/api/hello', function(req, res) {
 let urlObj = [];
 
 app.post('/api/shorturl', function(req, res) {
-  const urlRegex = /^(https?:\/\/)(www\.)?/;
-
+  const urlRegex = /^(http:\/\/www\.|https:\/\/www\.)\w*\.(com)$/;
+  
   if (urlRegex.test(req.body.url)) {
-    let newUrl;
-    try {
-      newUrl = new URL(req.body.url).hostname;
-    } catch (e) {
-      return res.json({ error: 'invalid url' });
-    }
-    console.log("This is the value of new URL", newUrl);
-
+    const newUrl = new URL(req.body.url).hostname;
     dns.lookup(newUrl, (error) => {
       if (error) {
-        return res.json({ error: 'Invalid Hostname' });
-      } else {
-        const foundObj = urlObj.find(obj => obj.original_url === req.body.url);
-        if (foundObj) {
-          return res.json({ original_url: req.body.url, short_url: foundObj.short_url });
-        } else {
-          let rand = urlObj.length + 1;
-          urlObj.push({
-            original_url: req.body.url,
-            short_url: rand
-          });
-          return res.json({ original_url: req.body.url, short_url: rand });
-        }
+       return res.json({ error: 'Invalid Hostname' });
       }
-    });
-  } else {
-    return res.json({ error: 'invalid url' });
+      else{
+      let shortUrl=isSameValuePresent(req.body.url, urlObj)
+        if (shortUrl) {
+       return res.json({ original_url: req.body.url, short_url: shortUrl });
+        
+      } else {
+        let rand = Math.floor(Math.random() * 100000);
+        urlObj.push({
+          original_url: req.body.url,
+          short_url: rand
+        });
+        return res.json({ original_url: req.body.url, short_url: rand });
+      }
+        
+      }
+    });  
+  } 
+  else{
+    return res.json({ error: 'invalid url' })
   }
+  
 });
 
 app.get('/api/shorturl/:short_url', function(req, res) {
   const linkNo = parseInt(req.params.short_url);
-
+  // Find the corresponding original_url based on short_url
   const foundObj = urlObj.find(obj => obj.short_url === linkNo);
   if (foundObj) {
-    return res.redirect(foundObj.original_url);
+     return res.redirect(foundObj.original_url );
   } else {
-    return res.json({ error: 'short_url not found' });
+   return res.json({ error: 'short_url not found' });
   }
 });
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+function isSameValuePresent(reqUrl, urlObj) {
+  const foundObj = urlObj.find(obj => obj.original_url === reqUrl);
+  return foundObj ? foundObj.short_url : false;
+}
